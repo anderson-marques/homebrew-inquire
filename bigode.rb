7149bf4a566d1a0b51e052a621b7e82a225048c7
Anderson Carvalho <anderson.mo.carvalho@gmail.com>
2020-05-24 15:59:13 +0100

    Updated the artefact url

--- bigode.rb
+++ bigode.rb
@@ -1,7 +1,7 @@
 class Bigode < Formula
   desc "Bigode is a CLI generator tool that takes a projectName, a templateFolder embeded with Mustache tags, and a context file in JSON and generates a new project."
   homepage ""
-  url "https://github.com/anderson-marques/pug-generator/releases/download/v1.0.0/pug-generator-macos"
+  url "https://github.com/anderson-marques/bigode/releases/download/v1.0.0/bigode-macos"
   sha256 "188d3dfa38482088587a557fe22139749f00943744f6952228994094ec373d3d"
 
   def install
