11b1e0a6ce5162533173740eb6e1d25a0db5625a
Anderson Carvalho <anderson.mo.carvalho@gmail.com>
2020-05-24 15:57:16 +0100

    Renamed puggen to bigode since it replaced pug to mustache

---
+++ bigode.rb
@@ -0,0 +1,10 @@
+class Bigode < Formula
+  desc "Bigode is a CLI generator tool that takes a projectName, a templateFolder embeded with Mustache tags, and a context file in JSON and generates a new project."
+  homepage ""
+  url "https://github.com/anderson-marques/pug-generator/releases/download/v1.0.0/pug-generator-macos"
+  sha256 "188d3dfa38482088587a557fe22139749f00943744f6952228994094ec373d3d"
+
+  def install
+    bin.install "bigode-macos" => "bigode"
+  end
+end
