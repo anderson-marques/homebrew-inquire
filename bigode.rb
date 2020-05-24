ae198c14c2109916344f1c50c1998b6d9e1a06ab
Anderson Carvalho <anderson.mo.carvalho@gmail.com>
2020-05-24 18:17:48 +0100

    Update bigode sha

--- bigode.rb
+++ bigode.rb
@@ -2,7 +2,7 @@ class Bigode < Formula
   desc "Bigode is a CLI generator tool that takes a projectName, a templateFolder embeded with Mustache tags, and a context file in JSON and generates a new project."
   homepage ""
   url "https://github.com/anderson-marques/bigode/releases/download/v1.0.0/bigode-macos"
-  sha256 "188d3dfa38482088587a557fe22139749f00943744f6952228994094ec373d3d"
+  sha256 "d98dd351866af3963d35c5ee5abfadc10f174809f28c462421f4a7a4378d1c12"
 
   def install
     bin.install "bigode-macos" => "bigode"
